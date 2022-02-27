3378b7216403ee2370f556edd90bf330c7a58826
Matt Draghi <mtd5264@gmail.com>
2022-02-26 19:35:54 -0500

    commit tree e3468593b2b5ba762556edea233064fe500a7bb9

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,47 @@
+require "pry"
 
+class CashRegister
+
+    attr_accessor :total, :last_transaction_price
+    attr_reader :discount, :items, :prices 
+
+    def initialize(discount=0)
+        @total = 0.0
+        @discount = discount.to_f
+        @items = []
+        @prices = []
+    end
+
+    def add_item(title, price, quantity=1)
+        @last_transaction_price = price * quantity
+        @total += (price * quantity)
+        if quantity == 1
+            @items.append(title)
+            @prices.append(price)
+        elsif quantity > 1
+            quantity.times do
+                @items.append(title)
+                @prices.append(price) 
+            end
+        end
+    end 
+
+    def apply_discount
+        if self.discount == 0.00
+            "There is no discount to apply."
+        else
+            self.total=(@total - (@total * (@discount/100)))
+            "After the discount, the total comes to $#{@total.to_i}."
+        end
+    end
+
+    def void_last_transaction
+        if @items.length > 0 #realistically, I would want to remove items and prices based off of a @quantity - which i never created
+            @items.pop
+            self.total = self.total - @last_transaction_price
+            @prices.pop
+            # binding.pry
+        end
+    end
+
+end
